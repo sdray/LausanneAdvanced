e2851776a023cf2ccc8cf5e3ffd18fa6d7679725
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2021-08-30 16:59:00 +0200

    R code to compile the course

---
+++ make.R
@@ -0,0 +1,24 @@
+# ----- clean workspace
+rm(list = ls())
+
+# ----- install/update packages
+devtools::install_deps()
+
+# ----- install compendium package
+devtools::install(build = FALSE)
+
+##build README.md and html
+rmarkdown::render(here::here("README.Rmd"))
+##build html
+rmarkdown::render(here::here("course/sessionXX","sessionXX.Rmd"))
+
+rmarkdown::render(here::here("practical/sessionXX","sessionXX.Rmd"))
+rmarkdown::render(here::here("practical/sessionXX","sessionXX.Rmd"),
+                  params = list(correction = TRUE), output_file = "sessionXX-corrected")
+
+
+## build pdf
+pagedown::chrome_print(here::here("course/sessionXX","sessionXX.html"))
+
+pagedown::chrome_print(here::here("practical/sessionXX","sessionXX.html"))
+pagedown::chrome_print(here::here("practical/sessionXX","sessionXX-corrected.html"))
