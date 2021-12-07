461192775b3470fbc998a0a575f318c5285e0eac
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2021-12-07 22:22:03 +0100

    Add session on two-table methods (course + tutorial) and recompile with updated make.R

--- make.R
+++ make.R
@@ -10,15 +10,16 @@ devtools::install(build = FALSE)
 ##build README.md and html
 rmarkdown::render(here::here("README.Rmd"))
 ##build html
-rmarkdown::render(here::here("course/session1","session1.Rmd"))
+rmarkdown::render(here::here("course/session0","session0.Rmd"))
+rmarkdown::render(here::here("course/session2","session2.Rmd"))
 
-rmarkdown::render(here::here("practical/sessionXX","sessionXX.Rmd"))
-rmarkdown::render(here::here("practical/sessionXX","sessionXX.Rmd"),
-                  params = list(correction = TRUE), output_file = "sessionXX-corrected")
+rmarkdown::render(here::here("practical/session2","session2.Rmd"))
+rmarkdown::render(here::here("practical/session2","session2.Rmd"),
+                  params = list(correction = TRUE), output_file = "session2-corrected")
 
 
 ## build pdf
-pagedown::chrome_print(here::here("course/session1","session1.html"))
+pagedown::chrome_print(here::here("course/session0","session0.html"))
 
-pagedown::chrome_print(here::here("practical/sessionXX","sessionXX.html"))
-pagedown::chrome_print(here::here("practical/sessionXX","sessionXX-corrected.html"))
+pagedown::chrome_print(here::here("practical/session2","session2.html"))
+pagedown::chrome_print(here::here("practical/session2","session2-corrected.html"))
