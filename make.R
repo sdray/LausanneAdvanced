49cb981a3b276786b7d8b05752274bdf83c9ff51
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2022-02-27 12:40:20 +0100

    Add new course on spatial analysis

--- make.R
+++ make.R
@@ -13,16 +13,17 @@ rmarkdown::render(here::here("README.Rmd"))
 rmarkdown::render(here::here("course/session0","session0.Rmd"))
 rmarkdown::render(here::here("course/session1","session1.Rmd"))
 rmarkdown::render(here::here("course/session2","session2.Rmd"))
+rmarkdown::render(here::here("course/session4","session4.Rmd"))
 
 rmarkdown::render(here::here("practical/session2","session2.Rmd"))
-rmarkdown::render(here::here("practical/session2","session2.Rmd"),
-                  params = list(correction = TRUE), output_file = "session2-corrected")
+rmarkdown::render(here::here("practical/session1","session1.Rmd"))
 
 
 ## build pdf
 pagedown::chrome_print(here::here("course/session0","session0.html"))
 pagedown::chrome_print(here::here("course/session1","session1.html"))
 pagedown::chrome_print(here::here("course/session2","session2.html"))
+pagedown::chrome_print(here::here("course/session4","session4.html"))
 
+pagedown::chrome_print(here::here("practical/session1","session1.html"))
 pagedown::chrome_print(here::here("practical/session2","session2.html"))
-pagedown::chrome_print(here::here("practical/session2","session2-corrected.html"))
