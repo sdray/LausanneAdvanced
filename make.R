10d48ce2648b9fbfbf7e81c044c663d4ab104264
Stéphane Dray <stephane.dray@univ-lyon1.fr>
2022-02-28 13:11:44 +0100

    Add course session 3

--- make.R
+++ make.R
@@ -13,17 +13,23 @@ rmarkdown::render(here::here("README.Rmd"))
 rmarkdown::render(here::here("course/session0","session0.Rmd"))
 rmarkdown::render(here::here("course/session1","session1.Rmd"))
 rmarkdown::render(here::here("course/session2","session2.Rmd"))
+rmarkdown::render(here::here("course/session3","session3.Rmd"))
 rmarkdown::render(here::here("course/session4","session4.Rmd"))
 
-rmarkdown::render(here::here("practical/session2","session2.Rmd"))
 rmarkdown::render(here::here("practical/session1","session1.Rmd"))
+rmarkdown::render(here::here("practical/session2","session2.Rmd"))
+rmarkdown::render(here::here("practical/session3","session3.Rmd"))
+rmarkdown::render(here::here("practical/session4","session4.Rmd"))
 
 
 ## build pdf
 pagedown::chrome_print(here::here("course/session0","session0.html"))
 pagedown::chrome_print(here::here("course/session1","session1.html"))
 pagedown::chrome_print(here::here("course/session2","session2.html"))
+pagedown::chrome_print(here::here("course/session3","session3.html"))
 pagedown::chrome_print(here::here("course/session4","session4.html"))
 
 pagedown::chrome_print(here::here("practical/session1","session1.html"))
 pagedown::chrome_print(here::here("practical/session2","session2.html"))
+pagedown::chrome_print(here::here("practical/session3","session3.html"))
+pagedown::chrome_print(here::here("practical/session4","session4.html"))
